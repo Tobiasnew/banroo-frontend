4e7e7d910da0c46102a0ff3fc95dfc2eeded44f1
Tobias Schütt <tobiasschuett6@googlemail.com>
2026-02-18 21:46:55 +0100

    feat: protected route, dashboard geschützt

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,25 @@ import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Match from "./pages/Match.jsx";
 import MatchResult from "./pages/MatchResult.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
     <Routes>
       <Route element={<SiteLayout />}>
         <Route index element={<Home />} />
-        <Route path="app" element={<Dashboard />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="match" element={<Match />} />
         <Route path="match/result" element={<MatchResult />} />
+        <Route
+          path="app"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
       </Route>
       <Route path="*" element={<Home />} />
     </Routes>
